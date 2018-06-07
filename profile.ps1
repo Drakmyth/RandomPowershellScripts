01f2d47fc660fb35722fe0fa0199ee3d689a342f
Drakmyth <shaun.hamman@gmail.com>
2018-06-07 10:22:18 -0700

    Added ability to load new keys without having to restart the ssh agent

--- profile.ps1
+++ profile.ps1
@@ -9,13 +9,21 @@ function tail { Get-Content $args -Wait }
 # Git
 Import-Module posh-git
 
+# SSH
 $UsingWindowsSSH = $false
 $AgentRunning = If ($UsingWindowsSSH) { Get-Service ssh-agent -ErrorAction SilentlyContinue } else { Get-SshAgent }
 If (-Not $AgentRunning) {
     If ($UsingWindowsSSH) { Start-Service ssh-agent } else { Start-SshAgent }
+}
 
-    ForEach ($item in Get-ChildItem ~/.ssh *.pub) {
-        Add-SshKey ~/.ssh/$($item.BaseName)
+$loadedKeys = Add-SshKey -l
+ForEach ($item in Get-ChildItem ~/.ssh *.pub) {
+
+    $privateKeyName = $item.BaseName
+    if ($loadedKeys -match "$($privateKeyName)") {
+        # TODO: Why doesn't -not or -notmatch work here?
+    } else {
+        Add-SshKey ~/.ssh/$($privateKeyName)
     }
 }
 
